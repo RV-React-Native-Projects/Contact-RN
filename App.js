12d645cfd02288c32bb5b0f75a58dbec38264e81
ranvijaykumar11 <ranvijay.kumar@presto-apps.com>
2021-10-16 17:14:16 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Heloo</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
